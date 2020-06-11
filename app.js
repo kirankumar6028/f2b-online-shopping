b77660c74627aec6f54d86223acd4153a716d5b2
kirankumar6028 <kiran6028kumar@gmail.com>
2020-06-11 23:36:19 +0530

    online shopping

---
+++ app.js
@@ -0,0 +1,58 @@
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const firebase = require("firebase");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extebded: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req,res){
+  res.render("index", {});
+});
+
+app.get("/admin", function(req, res){
+  res.render("admin", {});
+});
+
+app.get("/home", function(req, res){
+  res.render("home", {});
+});
+
+app.post("/home", function(req, res){
+  res.redirect("/home");
+});
+
+app.get("/customer", function(req, res){
+  res.render("customer", {});
+});
+
+app.post("/customer", function(){
+  res.redirect("/customer");
+});
+
+app.get("/products", function(req, res){
+  res.render("products", {});
+});
+
+app.post("/products", function(req, res){
+  res.redirect("/products");
+});
+
+app.get("/edit", function(req, res){
+  res.render("edit", {});
+});
+
+app.post("/edit", function(req, res){
+  res.redirect("/edit");
+});
+
+
+
+app.listen(3000, function(){
+  console.log("server running on port 3000");
+});
